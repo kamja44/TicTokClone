33f416f32c29ed0082eac3b693fb425529197977
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-06-15 09:46:27 +0900

    PageController

---
+++ lib/features/videos/widgets/video_post.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoPost extends StatefulWidget {
+  const VideoPost({super.key});
+
+  @override
+  State<VideoPost> createState() => VideoPostState();
+}
+
+class VideoPostState extends State<VideoPost> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
