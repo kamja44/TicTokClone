2efe6eec360ad8ee4f0e222d0f8a63eb2ffb988e
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-06-30 17:28:16 +0900

    Grid Item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = [
@@ -52,13 +54,62 @@ class DiscoverScreen extends StatelessWidget {
                 crossAxisCount: 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 22,
               ),
-              itemBuilder: (context, index) => Container(
-                color: Colors.teal,
-                child: Center(
-                  child: Text("$index"),
-                ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/potato.png",
+                        image:
+                            "https://avatars.githubusercontent.com/u/88872409?v=4"),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    "감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: Sizes.size16 + Sizes.size2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v10,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w600),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/88872409?v=4"),
+                        ),
+                        Gaps.h4,
+                        const Expanded(
+                          child: Text(
+                            "감자감자감자감자감자감자",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text(
+                          "2.5M",
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
             for (var tab in tabs.skip(1))
