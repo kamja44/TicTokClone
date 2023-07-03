b7490af7d6c9a4023f009714304467b696d41a28
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-07-03 11:32:20 +0900

    Code Challenge

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
@@ -22,140 +21,201 @@ class DiscoverScreen extends StatefulWidget {
 }
 
 class _DiscoverScreenState extends State<DiscoverScreen> {
-  final TextEditingController _textEditingController =
-      TextEditingController(text: "Initial Text");
-  void _onSearchChanged(String value) {
-    print("Search $value");
-  }
-
-  void _onSearchSubmitted(String value) {
-    print("Submitted $value");
-  }
-
+  final TextEditingController _textEditingController = TextEditingController();
+  bool _writing = false;
   @override
   void dispose() {
     _textEditingController.dispose();
     super.dispose();
   }
 
+  void _onWritingStart() {
+    setState(() {
+      _writing = true;
+    });
+  }
+
+  void _writingClear() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _textEditingController.clear();
+      _writing = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          elevation: 1,
-          title: CupertinoSearchTextField(
-            controller: _textEditingController,
-            onChanged: _onSearchChanged,
-            onSubmitted: _onSearchSubmitted,
-          ),
-          bottom: TabBar(
-            splashFactory: NoSplash.splashFactory,
-            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
-            isScrollable: true,
-            labelStyle: const TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: Sizes.size16,
+    return GestureDetector(
+      onPanDown: (value) {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            elevation: 1,
+            title: SizedBox(
+              height: Sizes.size36,
+              child: TextField(
+                controller: _textEditingController,
+                onTap: _onWritingStart,
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    hintStyle: TextStyle(
+                      fontSize: Sizes.size16,
+                      color: Colors.grey.shade500,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(Sizes.size6),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey.shade100,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size10,
+                    ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.only(left: Sizes.size8),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.magnifyingGlass,
+                            color: Colors.grey.shade500,
+                            size: Sizes.size20,
+                          ),
+                        ],
+                      ),
+                    ),
+                    prefixIconConstraints: const BoxConstraints(
+                      minWidth: 30,
+                    ),
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.only(right: Sizes.size8),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          GestureDetector(
+                            onTap: _writingClear,
+                            child: FaIcon(
+                              FontAwesomeIcons.solidCircleXmark,
+                              color: Colors.grey.shade500,
+                              size: Sizes.size20,
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              ),
             ),
-            indicatorColor: Colors.black,
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey.shade500,
-            tabs: [
-              for (var tab in tabs)
-                Tab(
-                  text: tab,
+            bottom: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+              isScrollable: true,
+              labelStyle: const TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: Sizes.size16,
+              ),
+              indicatorColor: Colors.black,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.shade500,
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                itemCount: 20,
+                padding: const EdgeInsets.all(
+                  Sizes.size6,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: Sizes.size10,
+                  mainAxisSpacing: Sizes.size10,
+                  childAspectRatio: 9 / 22,
+                ),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Sizes.size4),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder: "assets/images/potato.png",
+                            image:
+                                "https://avatars.githubusercontent.com/u/88872409?v=4"),
+                      ),
+                    ),
+                    Gaps.v10,
+                    const Text(
+                      "감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: Sizes.size16 + Sizes.size2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v10,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontWeight: FontWeight.w600),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            radius: 12,
+                            backgroundImage: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/88872409?v=4"),
+                          ),
+                          Gaps.h4,
+                          const Expanded(
+                            child: Text(
+                              "감자감자감자감자감자감자",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Gaps.h4,
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size16,
+                            color: Colors.grey.shade600,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "2.5M",
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              for (var tab in tabs.skip(1))
+                Center(
+                  child: Text(
+                    tab,
+                    style: const TextStyle(
+                      fontSize: 28,
+                    ),
+                  ),
                 ),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            GridView.builder(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              itemCount: 20,
-              padding: const EdgeInsets.all(
-                Sizes.size6,
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: Sizes.size10,
-                mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 22,
-              ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Sizes.size4),
-                    ),
-                    child: AspectRatio(
-                      aspectRatio: 9 / 16,
-                      child: FadeInImage.assetNetwork(
-                          fit: BoxFit.cover,
-                          placeholder: "assets/images/potato.png",
-                          image:
-                              "https://avatars.githubusercontent.com/u/88872409?v=4"),
-                    ),
-                  ),
-                  Gaps.v10,
-                  const Text(
-                    "감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자감자",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: Sizes.size16 + Sizes.size2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Gaps.v10,
-                  DefaultTextStyle(
-                    style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontWeight: FontWeight.w600),
-                    child: Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: 12,
-                          backgroundImage: NetworkImage(
-                              "https://avatars.githubusercontent.com/u/88872409?v=4"),
-                        ),
-                        Gaps.h4,
-                        const Expanded(
-                          child: Text(
-                            "감자감자감자감자감자감자",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Gaps.h4,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size16,
-                          color: Colors.grey.shade600,
-                        ),
-                        Gaps.h2,
-                        const Text(
-                          "2.5M",
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            for (var tab in tabs.skip(1))
-              Center(
-                child: Text(
-                  tab,
-                  style: const TextStyle(
-                    fontSize: 28,
-                  ),
-                ),
-              ),
-          ],
         ),
       ),
     );
